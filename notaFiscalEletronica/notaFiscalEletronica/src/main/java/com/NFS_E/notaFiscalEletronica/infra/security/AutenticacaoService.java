a08c6eb83bd5239ba6539b5463fd09c9a3c83bcb
joao <jovmamikl@gmail.com>
2026-03-09 14:44:34 -0300

    feat: implementado servico para autenticaçao do usuario

---
+++ notaFiscalEletronica/notaFiscalEletronica/src/main/java/com/NFS_E/notaFiscalEletronica/infra/security/AutenticacaoService.java
@@ -0,0 +1,21 @@
+package com.NFS_E.notaFiscalEletronica.infra.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.NFS_E.notaFiscalEletronica.repository.UsuarioRepository;
+
+@Service
+public class AutenticacaoService implements UserDetailsService {
+
+    @Autowired
+    private UsuarioRepository repository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return repository.findByLogin(username);
+    }
+}
