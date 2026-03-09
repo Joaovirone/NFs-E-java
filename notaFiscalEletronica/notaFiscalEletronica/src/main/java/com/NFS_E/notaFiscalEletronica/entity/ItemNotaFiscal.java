da5cb9d2ba8fcd95fcb63482cfc35912da5a996d
joao <jovmamikl@gmail.com>
2026-03-09 10:32:52 -0300

    feat: adicionado relação da Nota Fiscal com Item nota fiscal:
    @JoinColumn (name="nota_id")

--- notaFiscalEletronica/notaFiscalEletronica/src/main/java/com/NFS_E/notaFiscalEletronica/entity/ItemNotaFiscal.java
+++ notaFiscalEletronica/notaFiscalEletronica/src/main/java/com/NFS_E/notaFiscalEletronica/entity/ItemNotaFiscal.java
@@ -9,6 +9,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.Data;
 
@@ -52,6 +54,11 @@ public class ItemNotaFiscal {
 
     @Column(name= "valor_icms", nullable = false)
     private BigDecimal valorIcms;
+
+
+    @ManyToOne
+    @JoinColumn(name="nota_id")
+    private NotaFiscal notaFiscal;
 
 
     public ItemNotaFiscal(){}
