2e6b89b34a3eff9767c46f7e1938f97d428d025c
joao <jovmamikl@gmail.com>
2026-03-09 14:46:19 -0300

    feat: implementado filtro para a paginacao

--- notaFiscalEletronica/notaFiscalEletronica/src/main/java/com/NFS_E/notaFiscalEletronica/repository/NotaFiscalRepository.java
+++ notaFiscalEletronica/notaFiscalEletronica/src/main/java/com/NFS_E/notaFiscalEletronica/repository/NotaFiscalRepository.java
@@ -4,10 +4,11 @@ import java.util.UUID;
 
 import org.springframework.data.jdbc.repository.query.Query;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 import com.NFS_E.notaFiscalEletronica.entity.NotaFiscal;
 
-public interface NotaFiscalRepository extends JpaRepository <NotaFiscal, UUID>{
+public interface NotaFiscalRepository extends JpaRepository <NotaFiscal, UUID>, JpaSpecificationExecutor<NotaFiscal>{
     
     @Query("SELECT max(n.numero) FROM NotaFiscal n" )
     Long findMaxNumero();
