f9b08b2716f68eaf134d6c0e45e21099b46053cc
joao <jovmamikl@gmail.com>
2026-03-09 10:28:37 -0300

    feat: implementado emissão e listagem de todas as notas fiscais

--- notaFiscalEletronica/notaFiscalEletronica/src/main/java/com/NFS_E/notaFiscalEletronica/service/NotaFiscalService.java
+++ notaFiscalEletronica/notaFiscalEletronica/src/main/java/com/NFS_E/notaFiscalEletronica/service/NotaFiscalService.java
@@ -1,8 +1,14 @@
 package com.NFS_E.notaFiscalEletronica.service;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 
+import com.NFS_E.notaFiscalEletronica.controller.dto.NotaFiscalRequestDTO;
+import com.NFS_E.notaFiscalEletronica.controller.dto.NotaFiscalResponseDTO;
+import com.NFS_E.notaFiscalEletronica.controller.dto.mapper.NotaFiscalMapper;
 import com.NFS_E.notaFiscalEletronica.entity.NotaFiscal;
+import com.NFS_E.notaFiscalEletronica.entity.enums.StatusNota;
 import com.NFS_E.notaFiscalEletronica.repository.NotaFiscalRepository;
 
 import jakarta.transaction.Transactional;
@@ -13,14 +19,31 @@ import lombok.RequiredArgsConstructor;
 public class NotaFiscalService {
     
     private final NotaFiscalRepository repository;
+    private final NotaFiscalMapper mapper;
 
 
     @Transactional
-    public NotaFiscal emitir(NotaFiscal nota){
+    public NotaFiscalResponseDTO emitir( NotaFiscalRequestDTO dto){
 
+        NotaFiscal nota = mapper.toEntity(dto);
+
+        if(nota.getItens() != null ){
+            nota.getItens().forEach(item -> item.setNotaFiscal(nota));
+        }
+
+        nota.setStatus(StatusNota.PROCESSANDO);
         nota.calcularTotal();
 
-        return repository.save(nota);
+        NotaFiscal notaSalva = repository.save(nota);
+
+        return mapper.toResponse(notaSalva);
+
+
+    }
+
+    public List<NotaFiscalResponseDTO> listarTodas(){
+
+        return repository.findAll().stream().map(mapper :: toResponse).toList();
     }
 
     
