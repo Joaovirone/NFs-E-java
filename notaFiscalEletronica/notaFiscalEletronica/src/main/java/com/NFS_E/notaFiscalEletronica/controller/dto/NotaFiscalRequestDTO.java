f6a2503d6c8ed53e33d63a29e13266a34acc10e9
joao <jovmamikl@gmail.com>
2026-03-09 12:04:42 -0300

    feat: implementada anotações para validação de dTO

--- notaFiscalEletronica/notaFiscalEletronica/src/main/java/com/NFS_E/notaFiscalEletronica/controller/dto/NotaFiscalRequestDTO.java
+++ notaFiscalEletronica/notaFiscalEletronica/src/main/java/com/NFS_E/notaFiscalEletronica/controller/dto/NotaFiscalRequestDTO.java
@@ -2,6 +2,10 @@ package com.NFS_E.notaFiscalEletronica.controller.dto;
 
 import java.util.List;
 
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -11,6 +15,12 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class NotaFiscalRequestDTO{
-    String notaFsicalDestino;
-    List<ItemNotaFiscalRequestDTO> itens;
+
+    @NotBlank(message = "O destino da nota é obrigatório")
+    @Size(min = 3, max = 255, message = "O nome do destino deve ter entre 3 e 255 caracteres")
+    private String notaFiscalDestino;
+
+    @NotEmpty(message = "A nota fiscal deve conter pelo menos um item")
+    @Valid 
+    private List<ItemNotaFiscalRequestDTO> itens;
 }
