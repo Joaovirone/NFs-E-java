156fc77a1dfd90d74599574a35011341ed6302d7
joao <jovmamikl@gmail.com>
2026-03-09 10:30:27 -0300

    fat: criado config de model mapper com constructor vazio para remover boilerplate

---
+++ notaFiscalEletronica/notaFiscalEletronica/src/main/java/com/NFS_E/notaFiscalEletronica/config/ModelMapperConfig.java
@@ -0,0 +1,15 @@
+package com.NFS_E.notaFiscalEletronica.config;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ModelMapperConfig {
+    
+
+    @Bean
+    public ModelMapper modelMapper(){
+        return new ModelMapper();
+    }
+}
