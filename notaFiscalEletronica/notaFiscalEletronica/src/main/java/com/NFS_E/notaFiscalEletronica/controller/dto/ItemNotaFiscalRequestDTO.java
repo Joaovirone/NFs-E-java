bf049c0f43b0fc5aa3eac9a5cd2c9404e9ec7034
joao <jovmamikl@gmail.com>
2026-03-09 12:02:54 -0300

    feat: implementada anotações para tonar o DTO mais seguro e profssional

--- notaFiscalEletronica/notaFiscalEletronica/src/main/java/com/NFS_E/notaFiscalEletronica/controller/dto/ItemNotaFiscalRequestDTO.java
+++ notaFiscalEletronica/notaFiscalEletronica/src/main/java/com/NFS_E/notaFiscalEletronica/controller/dto/ItemNotaFiscalRequestDTO.java
@@ -2,6 +2,8 @@ package com.NFS_E.notaFiscalEletronica.controller.dto;
 
 import java.math.BigDecimal;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Positive;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -12,8 +14,17 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class ItemNotaFiscalRequestDTO{
    
+        @NotBlank(message="Descrição do Item é obrigatória")
+        private String descricao;
 
-        String descricao;
-        BigDecimal quantidade;
-        BigDecimal valorUnitari;
+        @Positive(message="A quantidade deve ser maior que zero")
+        private BigDecimal quantidade;
+
+        @Positive(message="O valor unitário deve ser positivo")
+        private BigDecimal valorUnitario;
+
+        private String ncm;         
+        private String cfop;         
+        private String cstIcms;      
+        private BigDecimal aliquotaIcms; 
 }
