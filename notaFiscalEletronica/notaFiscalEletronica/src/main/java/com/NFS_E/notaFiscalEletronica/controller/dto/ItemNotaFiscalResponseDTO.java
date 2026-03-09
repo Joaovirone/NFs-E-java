aebcc765852cbfcf386e0c32986540ff37713f95
João Vitor Pereira <153655175+Joaovirone@users.noreply.github.com>
2026-03-09 07:59:14 -0300

    feat: implementado responde do item nota fiscal

---
+++ notaFiscalEletronica/notaFiscalEletronica/src/main/java/com/NFS_E/notaFiscalEletronica/controller/dto/ItemNotaFiscalResponseDTO.java
@@ -0,0 +1,16 @@
+
+
+
+public record ItemNotaFiscalResponseDTO(
+
+    UUID id,
+    String descricao,
+    BigDecimal quantidade,
+    BigDecimal valorUnitario,
+    BigDecimal valorTotal
+    
+
+
+){
+
+}
