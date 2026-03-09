538d3da7b0e3e9586908c39afee91ac9e60c7d1f
joao <jovmamikl@gmail.com>
2026-03-09 12:05:08 -0300

    refactor: adicionado campos de auditoria e chaves de acesso

--- notaFiscalEletronica/notaFiscalEletronica/src/main/java/com/NFS_E/notaFiscalEletronica/entity/NotaFiscal.java
+++ notaFiscalEletronica/notaFiscalEletronica/src/main/java/com/NFS_E/notaFiscalEletronica/entity/NotaFiscal.java
@@ -1,15 +1,22 @@
 package com.NFS_E.notaFiscalEletronica.entity;
 
 import java.math.BigDecimal;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
 import com.NFS_E.notaFiscalEletronica.entity.enums.StatusNota;
+import com.fasterxml.jackson.annotation.JsonFormat;
 
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
@@ -23,12 +30,21 @@ import lombok.Data;
 @Entity
 @Table(name="notas_fiscais")
 @Data
+@EntityListeners(AuditingEntityListener.class)
 public class NotaFiscal {
     
     @Id
     @GeneratedValue(strategy= GenerationType.UUID)
     @Column(name= "id", nullable=false)
     private UUID id;
+
+    @Column(unique = true)
+    private Long numero;
+
+    private Integer serie = 1;
+
+    @Column(length= 44, unique = true)
+    private String chaveAcesso;
 
     @Column(name="nota_fiscal_destino", nullable=false)
     private String notaFiscalDestino;
@@ -44,11 +60,22 @@ public class NotaFiscal {
     private List<ItemNotaFiscal> itens = new ArrayList<>();
 
 
+    @CreatedDate
+    @Column(nullable=false, updatable=false)
+    @JsonFormat(pattern = "dd/MM/yyyy HH:mm:ss")
+    private LocalDateTime dataCriacao;
+
+    @LastModifiedDate
+    @JsonFormat(pattern = "dd/MM/yyyy HH:mm:ss")
+    private LocalDateTime dataUltimaAtualização;
+
+
     public void calcularTotal(){
 
         this.valorTotal = itens.stream()
                 .map(item -> {
                     item.calcularSubtotal();
+                    item.calcularImpostos();
                     return item.getValorTotal();
                 })
                 .reduce(BigDecimal.ZERO, BigDecimal :: add);
