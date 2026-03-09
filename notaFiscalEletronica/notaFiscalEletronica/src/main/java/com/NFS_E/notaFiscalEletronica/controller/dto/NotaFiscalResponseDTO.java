ff279dd035a682a27293d92a7ade95bce464ddd2
joao <jovmamikl@gmail.com>
2026-03-09 08:44:10 -0300

    fix: ajustada a vírgula que faltava causando erro no código

--- notaFiscalEletronica/notaFiscalEletronica/src/main/java/com/NFS_E/notaFiscalEletronica/controller/dto/NotaFiscalResponseDTO.java
+++ notaFiscalEletronica/notaFiscalEletronica/src/main/java/com/NFS_E/notaFiscalEletronica/controller/dto/NotaFiscalResponseDTO.java
@@ -1,11 +1,13 @@
 package com.NFS_E.notaFiscalEletronica.controller.dto;
 
 import java.math.BigDecimal;
+import java.util.List;
+import java.util.UUID;
 
 public record NotaFiscalResponseDTO(
 
         UUID id,
-        String notaFiscalDestino
+        String notaFiscalDestino,
         BigDecimal valorTotal,
         String status,
         List<ItemNotaFiscalResponseDTO> itens
